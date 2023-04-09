ac190072f3a694713124e055e3ea19f45b867624
Yair Yarom <yaromy@gmail.com>
2023-04-09 22:03:45 +0300

    acl: ALL -> *

--- lib/XMPputer/ACL.pm
+++ lib/XMPputer/ACL.pm
@@ -17,7 +17,7 @@ sub read_file {
     my $self = shift;
     my $file = shift;
 
-    $self->{acl} = {ALL => []};
+    $self->{acl} = {'*' => []};
     open(ACL, "<$file") or die "can't read acl file";
     foreach my $acl (<ACL>) {
 	my ($key, $value) = split /\s+/, $acl, 2;
@@ -34,10 +34,10 @@ sub allow {
     $self->{acl}{$command} //= [];
     return any { 0
 		   or $params->jid eq $_
-		   or "ALL" eq $_
+		   or "*" eq $_
 		   or ($params->room_member and $params->room_member eq $_)
 		   or ($params->room_member and bare_jid($params->room_member)."/*" eq $_)
-	       } (@{$self->{acl}{$command}}, @{$self->{acl}{ALL}});
+	       } (@{$self->{acl}{$command}}, @{$self->{acl}{'*'}});
 }
 
 1;
